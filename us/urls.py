04a6be45d0a45eac01322ad6c597dc003acf2c39
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-01 02:00:21 +0330

    >

--- us/urls.py
+++ us/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'us'
 urlpatterns = [
-    path("", views.Career.as_view(), name='career'),
-    path("", views.Customers.as_view(), name='customers'),
-    path("", views.Contactus.as_view(), name='contactus'),
-    path("", views.AboutUs.as_view(), name='aboutus'),
+    path("career/", views.Career.as_view(), name='career'),
+    path("custumers/", views.Customers.as_view(), name='customers'),
+    path("contactus/", views.Contactus.as_view(), name='contactus'),
+    path("aboutus/", views.AboutUs.as_view(), name='aboutus'),
 ]
