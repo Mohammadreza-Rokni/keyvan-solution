03f8352aa0bdc33c54114c33f3753976817d0146
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-02 20:34:06 +0330

    vebose name , admin.py of us

--- us/admin.py
+++ us/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# from . import  models
 
-# Register your models here.
+# # Register your models here.
+
+# admin.site.register(models.Contactus)
+# admin.site.register(models.Aboutus)
+# admin.site.register(models.Customer)
--- us/models.py
+++ us/models.py
@@ -39,17 +39,25 @@ class Contactus(models.Model):
   history = models.IntegerField()
   type_of_cooperation = type_of_cooperation_list
 
-
+  def __str__(self):
+    return self.title
 
 class Aboutus(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField()
   image = models.ImageField()
 
-
+  class Meta:
+        verbose_name = "درباره ما"
 
 class Customer(models.Model):
   logo = models.ImageField()
   title = models.CharField(max_length=100)
   description = models.TextField()
+
+  def __str__(self):
+        return self.title
   
+  class Meta:
+        verbose_name = "مشتری"
+        verbose_name_plural = "مشتریان"
