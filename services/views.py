6ac18ec5e779c14214787696373bf720c4d7a76c
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-03 18:18:37 +0330

    ,,,,

--- services/views.py
+++ services/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Oursolutions, Ourservices, Ourproducts
+from django.views.generic import DetailView, ListView
+from .models import Oursolutions, Ourservices, Ourproducts, Article
 # Create your views here.
-class OurSolutions(DetailView):
+class ArticleListView(ListView):
+    model = Article
+    context_object_name = "articles"
+    paginate_by = 4
+
+
+class OurSolutionsList(ListView):
+    model = Oursolutions
+    context_object_name = "oursolutions"
+    paginate_by = 1
+
+class OurSolutionsDetail(DetailView):
     template_name = 'index.html'
     model = Oursolutions
+
+
 
 class OurServices(DetailView):
     template_name = 'index.html'
