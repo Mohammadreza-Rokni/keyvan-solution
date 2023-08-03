4ff4faabd3c54aaaad5edd584675024a4b1c08c7
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-03 18:40:41 +0330

    urls ,views of services app

--- services/urls.py
+++ services/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 app_name = 'services'
 urlpatterns = [
-    path("", views.OurSolutions.as_view(), name='oursolutions'),
-    path("", views.OurServices.as_view(), name='ourservices'),
-    path("", views.OurProducts.as_view(), name='ourproducts'),
+    path("articleslist", views.ArticleListView.as_view(), name='article_list'),
+    path("articledetail/<str:slug>", views.ArticleDetailView.as_view(), name='articles_detail'),
+    path("category/<int:pk>", views.category_detail, name='category_detail'),
+    path("solutionslist", views.OurSolutionsListView.as_view(), name='oursolutions_list'),
+    path("solutiondetail/<str:slug>", views.OurSolutionsDetailView.as_view(), name='oursolution_detail'),
+    path("serviceslist", views.OurServicesListView.as_view(), name='ourservices_list'),
+    path("servicedetail/<str:slug>", views.OurServicesDetailView.as_view(), name='ourservice_detail'),
+    path("productslist", views.OurProductsListView.as_view(), name='ourproducts_list'),
+    path("productdetail/<str:slug>", views.OurProductsDetailView.as_view(), name='ourproduct_detail'),
 ]
--- services/views.py
+++ services/views.py
@@ -1,28 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import DetailView, ListView
-from .models import Oursolutions, Ourservices, Ourproducts, Article
+from .models import Oursolutions, Ourservices, Ourproducts, Article, Category
 # Create your views here.
 class ArticleListView(ListView):
     model = Article
     context_object_name = "articles"
     paginate_by = 4
 
+class ArticleDetailView(DetailView):
+    template_name = 'index.html'
+    model = Article
 
-class OurSolutionsList(ListView):
+def category_detail(request, pk=None):
+    category = get_object_or_404(Category, id=pk)
+    articles = category.articles.all()
+    return render(request, 'index.html', {'articles': articles})
+
+class OurSolutionsListView(ListView):
     model = Oursolutions
     context_object_name = "oursolutions"
-    paginate_by = 1
+    paginate_by = 4
 
-class OurSolutionsDetail(DetailView):
+class OurSolutionsDetailView(DetailView):
     template_name = 'index.html'
     model = Oursolutions
 
+class OurServicesListView(ListView):
+    model = Ourservices
+    context_object_name = "ourservices"
+    paginate_by = 4
 
-
-class OurServices(DetailView):
+class OurServicesDetailView(DetailView):
     template_name = 'index.html'
     model = Ourservices
 
-class OurProducts(DetailView):
+class OurProductsListView(ListView):
+    model = Ourproducts
+    context_object_name = "ourproducts"
+    paginate_by = 4
+
+class OurProductsDetailView(DetailView):
     template_name = 'index.html'
     model = Ourproducts
