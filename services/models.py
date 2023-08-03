0cd3d201a244be75f0db2f4c239f0fb69c96352a
Sajjad Sheykhi <sajjad.sheykhi.2004@gmail.com>
2023-08-03 18:08:13 +0330

    add article model

--- services/models.py
+++ services/models.py
@@ -68,3 +68,42 @@ class Ourproducts(models.Model):
     class Meta:
         verbose_name = "محصولات ما"
         verbose_name_plural = "محصولات ما"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField()
+    publish = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    Category = models.ManyToManyField(Category, related_name='articles')
+    slug = models.SlugField(blank=True, unique=True)
+
+    def save(
+            self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.slug = slugify(self.title)
+        super(Article, self).save()
+
+    class Meta:
+        ordering = ('-create', '-update')
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
+
+    def get_absolute_url(self):
+        return reverse('html_page', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return f"{self.title} - {self.body[:30]}"
