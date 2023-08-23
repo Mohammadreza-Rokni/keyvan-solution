d73c0dddaa86f1108c6b0ccf57c0599cc7a60344
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-24 01:15:52 +0330

    loading page

--- k1-front/script.js
+++ k1-front/script.js
@@ -30,6 +30,15 @@ function changeSlide() {
 }
 setInterval(changeSlide, 3000);
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Simulate the delay in loading your content (remove this in production)
+  setTimeout(function() {
+    document.querySelector(".loading-screen").style.display = "none"; // Hide loading screen
+    document.querySelector("header").style.display = "block"; // Show the navigation bar
+  }, 2000); // Change the duration as needed
+});
+
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   const filterDropdown = document.getElementById("filterDropdown");
 //   const filterItems = document.querySelectorAll(".filter-item");
