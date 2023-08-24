c453f5a053f4578953dfb76c18c8aaed00d6d1f9
Sajjad Sheykhi <sajjad.sheykhi.2004@gmail.com>
2023-08-24 20:35:55 +0330

    load static

--- keyvansolution/settings.py
+++ keyvansolution/settings.py
@@ -58,8 +58,7 @@ ROOT_URLCONF = 'keyvansolution.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR ,"static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
