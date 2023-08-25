5df30bc742b17ebd493266c1d170bcda36c11203
MohammadRezaRokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-08-26 01:59:55 +0330

    blog in home

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from services.models import Article
 # Create your views here.
 
-class Home(TemplateView):
+class HomeListView(ListView):
     template_name = 'home/home.html'
+    model = Article
+    context_object_name = "articles"
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+
+    #     sort = self.request.GET.get('sort')
+    #     if sort == 'newest':
+    #         queryset = queryset.order_by('-publish', '-update')
+    #     elif sort == 'oldest':
+    #         queryset = queryset.order_by('publish', 'update')
+
+    #     return queryset
